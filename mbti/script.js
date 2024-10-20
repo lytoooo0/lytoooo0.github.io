d3d132f7cc6440f712839633a45d6d094e2a4399
lytoooo0 <lytoooo0@icloud.com>
2024-10-21 00:24:09 +0800

    feat: adding mbti tests

---
+++ mbti/script.js
@@ -0,0 +1,34 @@
+document.getElementById('quizForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let result = '';
+    const answers = {
+        E: 0,
+        I: 0,
+        S: 0,
+        N: 0,
+        T: 0,
+        F: 0,
+        J: 0,
+        P: 0
+    };
+
+    // Collect answers
+    for (let i = 1; i <= 20; i++) {
+        const question = document.querySelector(`input[name="q${i}"]:checked`);
+        if (question) {
+            answers[question.value]++;
+        }
+    }
+
+    // Determine personality type
+    const personalityType = 
+        (answers.E > answers.I ? 'E' : 'I') +
+        (answers.S > answers.N ? 'S' : 'N') +
+        (answers.T > answers.F ? 'T' : 'F') +
+        (answers.J > answers.P ? 'J' : 'P');
+
+    // Display result
+    result = `Your personality type is: ${personalityType}`;
+    document.getElementById('result').innerText = result;
+});
